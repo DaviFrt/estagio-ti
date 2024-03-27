59e976a8bd14397b81e8ef7c97441888177b9dbb
DaviFrt <daviamomfb@gmail.com>
2024-03-27 14:58:33 -0300

    feat: add empty stock message

--- src/app/components/products.tsx
+++ src/app/components/products.tsx
@@ -10,20 +10,30 @@ export function Products() {
   const [products] = useState<Product[]>(ProductsInMemory)
 
   return (
-    <div className="grid grid-cols-5 gap-4">
-      {products.map((product, index) => (
-        <Card className="max-w-72" key={index}>
-          <CardHeader>
-            <h3>{product.name}</h3>
-          </CardHeader>
-          <CardContent className="flex items-center justify-between">
-            <span className="text-sm">
-              {product.quantity} {product.quantity > 1 ? 'itens' : 'item'}
-            </span>
-            <span className="text-sm">{formatCurrency(product.unitPrice)}</span>
-          </CardContent>
-        </Card>
-      ))}
+    <div>
+      {products.length > 0 ? (
+        <div className="grid grid-cols-5 gap-4">
+          {products.map((product, index) => (
+            <Card className="max-w-72" key={index}>
+              <CardHeader>
+                <h3>{product.name}</h3>
+              </CardHeader>
+              <CardContent className="flex items-center justify-between">
+                <span className="text-sm">
+                  {product.quantity} {product.quantity > 1 ? 'itens' : 'item'}
+                </span>
+                <span className="text-sm">
+                  {formatCurrency(product.unitPrice)}
+                </span>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      ) : (
+        <h2 className="text-lg">
+          Ops... parece que estamos sem produtos no estoque!
+        </h2>
+      )}
     </div>
   )
 }
